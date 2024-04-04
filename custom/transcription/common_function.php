6247d63229128e46784db3bfe92411670fae7b89
tasim313 <mostasimmahmudtasim@gmail.com>
2024-04-04 04:53:57 +0000

    create function

--- custom/transcription/common_function.php
+++ custom/transcription/common_function.php
@@ -1,41 +1,39 @@
 <?php 
 include('connection.php');
 
-function getUserGroupNames($loggedInUserId) {
-    global $pg_con;
-    
-    // Prepare the SQL query with a placeholder for the user ID
-    $sql = "SELECT ug.nom
-            FROM llx_usergroup ug
-            JOIN llx_usergroup_user ugu ON ug.rowid = ugu.fk_usergroup
-            JOIN llx_user u ON u.rowid = ugu.fk_user
-            WHERE u.rowid = $1"; // Placeholder for the user ID
-    
-    // Prepare the statement
-    $stmt = pg_prepare($pg_con, "", $sql);
-    
-    // Execute the statement with the user ID parameter
-    $result = pg_execute($pg_con, "", array($loggedInUserId));
-    
-    $userGroupNames = [];
-    if ($result) {
-        while ($row = pg_fetch_assoc($result)) {
-            $userGroupNames[] = ['group' => $row['nom']];
-        }
-        pg_free_result($result);
-    } else {
-        echo 'Error: ' . pg_last_error($pg_con);
-    }
-    return $userGroupNames;
-}
-
 
 function get_done_gross_list() {
     global $pg_con;
 
-    $sql = "SELECT gross_id, lab_number, patient_code, gross_assistant_name, gross_doctor_name
-            FROM llx_gross
-            WHERE gross_status = 'Done' AND gross_is_completed = 'true'";
+    $sql = "SELECT g.gross_id,
+    g.lab_number,
+    g.patient_code,
+    g.gross_assistant_name, 
+    g.gross_doctor_name
+    FROM llx_gross g
+        WHERE g.gross_status = 'Done' 
+        AND g.gross_is_completed = 'true'
+        AND NOT EXISTS (
+        SELECT 1
+        FROM llx_micro m
+        WHERE g.gross_id = CAST(m.fk_gross_id AS INTEGER)
+        )
+        AND NOT EXISTS (
+        SELECT 1
+        FROM llx_diagnosis d
+        WHERE g.gross_id = CAST(d.fk_gross_id AS INTEGER)
+        )
+        AND NOT EXISTS (
+        SELECT 1
+        FROM llx_micro m
+        WHERE g.lab_number = m.lab_number
+        )
+        AND NOT EXISTS (
+        SELECT 1
+        FROM llx_diagnosis d
+        WHERE g.lab_number = d.lab_number
+        );
+    ";
 
     $result = pg_query($pg_con, $sql);
 
@@ -60,37 +58,164 @@ function get_done_gross_list() {
     return $done_list;
 }
 
-
-function get_gross_specimens_list($lab_number) {
+function get_pending_transcription_value() {
     global $pg_con;
 
-    $sql = "SELECT de.fk_commande, de.fk_product, de.description as specimen,  c.ref, e.num_containers,
-    (
-        SELECT COUNT(*) 
-        FROM llx_commandedet AS inner_de 
-        WHERE inner_de.fk_commande = c.rowid
-    ) AS number_of_specimens
-FROM 
-    llx_commande AS c 
-JOIN 
-    llx_commandedet AS de ON de.fk_commande = c.rowid
-JOIN 
-    llx_commande_extrafields AS e ON e.fk_object = c.rowid
-WHERE 
-    c.ref = '$lab_number'";
+    $sql = "SELECT COUNT(*) AS total_count
+    FROM (
+        SELECT g.gross_id,
+               g.lab_number,
+               g.patient_code,
+               g.gross_assistant_name, 
+               g.gross_doctor_name
+        FROM llx_gross g
+        WHERE g.gross_status = 'Done' 
+          AND g.gross_is_completed = 'true'
+          AND NOT EXISTS (
+              SELECT 1
+              FROM llx_micro m
+              WHERE g.gross_id = CAST(m.fk_gross_id AS INTEGER)
+          )
+          AND NOT EXISTS (
+              SELECT 1
+              FROM llx_diagnosis d
+              WHERE g.gross_id = CAST(d.fk_gross_id AS INTEGER)
+          )
+          AND NOT EXISTS (
+              SELECT 1
+              FROM llx_micro m
+              WHERE g.lab_number = m.lab_number
+          )
+          AND NOT EXISTS (
+              SELECT 1
+              FROM llx_diagnosis d
+              WHERE g.lab_number = d.lab_number
+          )
+    ) AS subquery;";
+    $result = pg_query($pg_con, $sql);
+
+    if ($result) {
+        $row = pg_fetch_assoc($result);
+        $count = $row['total_count'];
+        pg_free_result($result);
+    } else {
+        echo 'Error: ' . pg_last_error($pg_con);
+        $count = 0; 
+    }
+
+    return $count;
+}
+
+function get_complete_transcription_value() {
+    global $pg_con;
+
+    $sql = "SELECT COUNT(*) AS total_count
+    FROM (
+        SELECT g.gross_id,
+               g.lab_number,
+               g.patient_code,
+               g.gross_assistant_name, 
+               g.gross_doctor_name
+        FROM llx_gross g
+        WHERE g.gross_status = 'Done' 
+          AND g.gross_is_completed = 'true'
+          AND  EXISTS (
+              SELECT 1
+              FROM llx_micro m
+              WHERE g.gross_id = CAST(m.fk_gross_id AS INTEGER)
+          )
+          AND  EXISTS (
+              SELECT 1
+              FROM llx_diagnosis d
+              WHERE g.gross_id = CAST(d.fk_gross_id AS INTEGER)
+          )
+          AND  EXISTS (
+              SELECT 1
+              FROM llx_micro m
+              WHERE g.lab_number = m.lab_number
+          )
+          AND  EXISTS (
+              SELECT 1
+              FROM llx_diagnosis d
+              WHERE g.lab_number = d.lab_number
+          )
+    ) AS subquery;";
+    $result = pg_query($pg_con, $sql);
+
+    if ($result) {
+        $row = pg_fetch_assoc($result);
+        $count = $row['total_count'];
+        pg_free_result($result);
+    } else {
+        echo 'Error: ' . pg_last_error($pg_con);
+        $count = 0; 
+    }
+
+    return $count;
+}
+
+
+function get_complete_transcription_value_current_month() {
+    global $pg_con;
+
+    $sql = "SELECT COUNT(DISTINCT fk_gross_id) AS micro_count
+    FROM llx_micro
+    WHERE EXTRACT(MONTH FROM create_date) = EXTRACT(MONTH FROM CURRENT_DATE)";
+    $result = pg_query($pg_con, $sql);
+
+    if ($result) {
+        $row = pg_fetch_assoc($result);
+        $count = $row['micro_count'];
+        pg_free_result($result);
+    } else {
+        echo 'Error: ' . pg_last_error($pg_con);
+        $count = 0; 
+    }
+
+    return $count;
+}
+
+
+function get_complete_transcription_value_current_year() {
+    global $pg_con;
+
+    $sql = "SELECT COUNT(DISTINCT fk_gross_id) AS micro_count
+    FROM llx_micro
+    WHERE EXTRACT(YEAR FROM create_date) = EXTRACT(YEAR FROM CURRENT_DATE);";
+    $result = pg_query($pg_con, $sql);
+
+    if ($result) {
+        $row = pg_fetch_assoc($result);
+        $count = $row['micro_count'];
+        pg_free_result($result);
+    } else {
+        echo 'Error: ' . pg_last_error($pg_con);
+        $count = 0; 
+    }
+
+    return $count;
+}
+
+function get_micro_description($fk_gross_id, $lab_number) {
+    global $pg_con;
+
+    $sql = "select row_id, lab_number, fk_gross_id, description, status, specimen from llx_micro where fk_gross_id='$fk_gross_id' 
+    AND lab_number = '$lab_number'";
 
     $result = pg_query($pg_con, $sql);
 
-    $specimens = [];
+    $description_list = [];
 
     if ($result) {
         while ($row = pg_fetch_assoc($result)) {
-            $specimens[] = [
-                'fk_commande' =>$row['fk_commande'] , 
-                'num_containers' => $row['num_containers'], 
-                'fk_product' => $row['fk_product'], 
-                'number_of_specimens' => $row['number_of_specimens'],
-                'specimen' => $row['specimen']];
+            $description_list[] = [
+                'row_id' => $row['row_id'],
+                'lab_number' => $row['lab_number'],
+                'fk_gross_id' => $row['fk_gross_id'],
+                'description' => $row['description'],
+                'status' => $row['status'],
+                'specimen' => $row['specimen']
+            ];
         }
 
         pg_free_result($result);
@@ -98,7 +223,11 @@ WHERE
         echo 'Error: ' . pg_last_error($pg_con);
     }
 
-    return $specimens;
+    return $description_list;
 }
+
+
+
+
 
 ?>
