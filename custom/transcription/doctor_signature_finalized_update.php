51da0d67ca1b84b2b05b9ae9f3fb3208929b5323
tasim313 <mostasimmahmudtasim@gmail.com>
2024-04-15 12:19:05 +0000

    create doctor sign

---
+++ custom/transcription/doctor_signature_finalized_update.php
@@ -0,0 +1,33 @@
+<?php
+include("connection.php");
+
+// Check if all required fields are present
+$required_fields = ['row_id', 'doctor_username', 'lab_number',  'created_user'];
+$missing_fields = array_diff_key(array_flip($required_fields), $_POST);
+if (!empty($missing_fields)) {
+    echo "Error: Missing required inputs: " . implode(', ', array_keys($missing_fields));
+    exit();
+}
+
+// Extract data from the POST request
+$row_id = $_POST['row_id'];
+$doctor_username = $_POST['doctor_username'];
+$lab_number = $_POST['lab_number'];
+$created_user = $_POST['created_user'];
+
+// Update the doctor signature
+$update_query = "UPDATE llx_doctor_finalized_by_signature  SET doctor_username = $2, lab_number = $3, created_user = $4 WHERE row_id = $1";
+$update_result = pg_query_params($pg_con, $update_query, array($row_id, $doctor_username, $lab_number, $created_user));
+
+if (!$update_result) {
+    error_log("Error updating doctor details: " . pg_last_error($pg_con));
+    echo "Error updating doctor details.";
+    exit();
+}
+
+echo "Doctor signature updated successfully.";
+header("Location: doctor_signature.php?lab_number=$lab_number");
+
+// Close the database connection
+pg_close($pg_con);
+?>
