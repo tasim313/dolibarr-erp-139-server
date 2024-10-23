c33d31cbc6397a94b81e8f98c56aae400190169d
tasim313 <mostasimmahmudtasim@gmail.com>
2024-10-23 06:15:50 +0000

    add gross station

--- custom/histolab/histo_common_function.php
+++ custom/histolab/histo_common_function.php
@@ -5,7 +5,7 @@ function get_histo_gross_specimen_list() {
     global $pg_con;
 
     $sql = "SELECT g.gross_id, g.lab_number, g.gross_create_date, g.gross_status, g.gross_doctor_name as doctor, g.gross_assistant_name as assistant,
-            s.gross_specimen_section_id, s.section_code, s.cassettes_numbers, s.tissue, s.requires_slide_for_block
+            g.gross_station_type, s.gross_specimen_section_id, s.section_code, s.cassettes_numbers, s.tissue, s.requires_slide_for_block
             FROM llx_gross g
             INNER JOIN llx_gross_specimen_section s ON g.gross_id = CAST(s.fk_gross_id AS INTEGER)
             WHERE g.gross_status = 'Done'
@@ -19,7 +19,8 @@ function get_histo_gross_specimen_list() {
             $gross_specimens[] = ['Lab Number' => $row['lab_number'], 'Gross Create Date' => $row['gross_create_date'],
             'Gross Status'=>$row['gross_status'], 'gross_specimen_section_id' => $row['gross_specimen_section_id'], 
             'section_code' => $row['section_code'], 'cassettes_numbers' => $row['cassettes_numbers'], 'tissue' => $row['tissue'],
-            'doctor' => $row['doctor'], 'assistant' => $row['assistant'], 'requires_slide_for_block' => $row['requires_slide_for_block']
+            'doctor' => $row['doctor'], 'assistant' => $row['assistant'], 'requires_slide_for_block' => $row['requires_slide_for_block'],
+            'gross_station_type' => $row['gross_station_type'],
         ];
         }
 
