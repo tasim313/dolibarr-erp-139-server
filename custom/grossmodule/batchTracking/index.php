902e090f955c554b6463fbd5d5ec7ade292303b4
tasim313 <mostasimmahmudtasim@gmail.com>
2024-10-24 06:29:21 +0000

    Add new feature                                                                                                                                                              Co-authored-by: prantorishere <sharifrahman256@gmail.com>

---
+++ custom/grossmodule/batchTracking/index.php
@@ -0,0 +1,66 @@
+<?php 
+
+include("../connection.php");
+include("../gross_common_function.php");
+
+$res = 0;
+
+if (!$res && !empty($_SERVER["CONTEXT_DOCUMENT_ROOT"])) {
+	$res = @include $_SERVER["CONTEXT_DOCUMENT_ROOT"]."/main.inc.php";
+}
+
+$tmp = empty($_SERVER['SCRIPT_FILENAME']) ? '' : $_SERVER['SCRIPT_FILENAME']; $tmp2 = realpath(__FILE__); $i = strlen($tmp) - 1; $j = strlen($tmp2) - 1;
+while ($i > 0 && $j > 0 && isset($tmp[$i]) && isset($tmp2[$j]) && $tmp[$i] == $tmp2[$j]) {
+	$i--; $j--;
+}
+if (!$res && $i > 0 && file_exists(substr($tmp, 0, ($i + 1))."/main.inc.php")) {
+	$res = @include substr($tmp, 0, ($i + 1))."/main.inc.php";
+}
+if (!$res && $i > 0 && file_exists(dirname(substr($tmp, 0, ($i + 1)))."/main.inc.php")) {
+	$res = @include dirname(substr($tmp, 0, ($i + 1)))."/main.inc.php";
+}
+
+if (!$res && file_exists("../main.inc.php")) {
+	$res = @include "../main.inc.php";
+}
+if (!$res && file_exists("../../main.inc.php")) {
+	$res = @include "../../main.inc.php";
+}
+if (!$res && file_exists("../../../main.inc.php")) {
+	$res = @include "../../../main.inc.php";
+}
+if (!$res) {
+	die("Include of main fails");
+}
+
+require_once DOL_DOCUMENT_ROOT.'/core/class/html.formcompany.class.php';
+require_once DOL_DOCUMENT_ROOT.'/core/class/html.formfile.class.php';
+require_once DOL_DOCUMENT_ROOT.'/core/class/html.formprojet.class.php';
+dol_include_once('/grossmodule/class/gross.class.php');
+dol_include_once('/grossmodule/lib/grossmodule_gross.lib.php');
+
+$title = $langs->trans("Gross Abbrevations Insert");
+$help_url = '';
+llxHeader('', $title, $help_url);
+
+$loggedInUsername = $user->login;
+$loggedInUserId = $user->id;
+
+
+
+$isAdmin = isUserAdmin($loggedInUserId);
+
+
+// Access control using switch statement
+switch (true) {
+    case $isAdmin:
+        // Admin has access, continue with the page content...
+        break;
+    
+    default:
+        echo "<h1>Access Denied</h1>";
+        echo "<p>You are not authorized to view this page.</p>";
+        exit; // Terminate script execution
+}
+
+?>
