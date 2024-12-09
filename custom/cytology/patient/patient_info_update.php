060cc47d657b170cfea46ccba828a1403031f71b
tasim313 <mostasimmahmudtasim@gmail.com>
2024-12-09 11:53:15 +0000

    add new option to update data

---
+++ custom/cytology/patient/patient_info_update.php
@@ -0,0 +1,107 @@
+<?php 
+include('../connection.php');
+include('../../grossmodule/gross_common_function.php');
+include('../../transcription/common_function.php');
+include('../common_function.php');
+
+$res = 0;
+// Try main.inc.php into web root known defined into CONTEXT_DOCUMENT_ROOT (not always defined)
+if (!$res && !empty($_SERVER["CONTEXT_DOCUMENT_ROOT"])) {
+	$res = @include $_SERVER["CONTEXT_DOCUMENT_ROOT"]."/main.inc.php";
+}
+// Try main.inc.php into web root detected using web root calculated from SCRIPT_FILENAME
+$tmp = empty($_SERVER['SCRIPT_FILENAME']) ? '' : $_SERVER['SCRIPT_FILENAME']; $tmp2 = realpath(__FILE__); $i = strlen($tmp) - 1; $j = strlen($tmp2) - 1;
+while ($i > 0 && $j > 0 && isset($tmp[$i]) && isset($tmp2[$j]) && $tmp[$i] == $tmp2[$j]) {
+	$i--; $j--;
+}
+if (!$res && $i > 0 && file_exists(substr($tmp, 0, ($i + 1))."/main.inc.php")) {
+	$res = @include substr($tmp, 0, ($i + 1))."/main.inc.php";
+}
+if (!$res && $i > 0 && file_exists(dirname(substr($tmp, 0, ($i + 1)))."/main.inc.php")) {
+	$res = @include dirname(substr($tmp, 0, ($i + 1)))."/main.inc.php";
+}
+// Try main.inc.php using relative path
+if (!$res && file_exists("../main.inc.php")) {
+	$res = @include "../main.inc.php";
+}
+if (!$res && file_exists("../../main.inc.php")) {
+	$res = @include "../../main.inc.php";
+}
+if (!$res && file_exists("../../../main.inc.php")) {
+	$res = @include "../../../main.inc.php";
+}
+if (!$res) {
+	die("Include of main fails");
+}
+
+require_once DOL_DOCUMENT_ROOT.'/core/class/html.formfile.class.php';
+
+// Load translation files required by the page
+$langs->loadLangs(array("cytology@cytology"));
+
+$action = GETPOST('action', 'aZ09');
+
+
+$socid = GETPOST('socid', 'int');
+if (isset($user->socid) && $user->socid > 0) {
+	$action = '';
+	$socid = $user->socid;
+}
+
+$max = 5;
+$now = dol_now();
+
+
+$form = new Form($db);
+$formfile = new FormFile($db);
+
+llxHeader("", $langs->trans("CytologyArea"));
+
+
+
+$loggedInUsername = $user->login;
+$loggedInUserId = $user->id;
+
+$isCytoAssistant = false;
+$isDoctor = false;
+
+$isAdmin = isUserAdmin($loggedInUserId);
+
+$LabNumber = $_GET['LabNumber'];
+
+
+$assistants = get_cyto_tech_list();
+foreach ($assistants as $assistant) {
+    if ($assistant['username'] == $loggedInUsername) {
+        $isCytoAssistant = true;
+        break;
+    }
+}
+
+$doctors = get_doctor_list();
+foreach ($doctors as $doctor) {
+    if ($doctor['doctor_username'] == $loggedInUsername) {
+        $isDoctor = true;
+        break;
+    }
+}
+
+// Access control using switch statement
+switch (true) {
+    case $isCytoAssistant:
+        // cyto Assistant has access, continue with the page content...
+        break;
+    case $isDoctor:
+        // Doctor has access, continue with the page content...
+        break;
+    case $isAdmin:
+        // Admin has access, continue with the page content...
+        break;
+        
+    default:
+        echo "<h1>Access Denied</h1>";
+        echo "<p>You are not authorized to view this page.</p>";
+        exit; // Terminate script execution
+}
+
+?>
